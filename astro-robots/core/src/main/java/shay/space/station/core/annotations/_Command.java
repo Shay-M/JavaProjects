4fbafd53da53d849dfc58a86e5fc8216506c1ee7
“ShayM” <shay1mualem@gmail.com>
2023-08-15 21:52:39 +0300

    add annotations

---
+++ astro-robots/core/src/main/java/shay/space/station/core/annotations/_Command.java
@@ -0,0 +1,17 @@
+package shay.space.station.core.annotations;
+
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+@Qualifier
+public @interface _Command {
+}
