5178c65877e1869044e59afa45c1d408100bbca9
“ShayM” <shay1mualem@gmail.com>
2023-08-16 16:08:08 +0300

    change to use ListableBeanFactory

--- astro-robots/robotics-robots/src/main/java/shay/space/station/robots/factory/RobotModelConfig.java
+++ astro-robots/robotics-robots/src/main/java/shay/space/station/robots/factory/RobotModelConfig.java
@@ -1,25 +1,48 @@
 package shay.space.station.robots.factory;
 
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import shay.space.station.infra.base.Robot;
 import shay.space.station.robots.models.Hal9000;
+import shay.space.station.robots.models.Johnny5;
+import shay.space.station.robots.models.Maschinenmensch;
+import shay.space.station.robots.models.Tachikomas;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
 @Configuration
 public class RobotModelConfig {
 
+    @Autowired
+    List<Robot> robots;
+
+    // @Bean
+    // public Map<String, Class<? extends Robot>> robotTypes() {
+    //     Map<String, Class<? extends Robot>> robotTypes = new HashMap<>();
+    //     robotTypes.put("HAL9000", Hal9000.class);
+    //     robotTypes.put("TACHIKOMAS", Tachikomas.class);
+    //     robotTypes.put("JOHNNY5", Johnny5.class);
+    //     robotTypes.put("MASCHINENMENSCH", Maschinenmensch.class);
+    //     // Add more robot models here
+    //     return robotTypes;
+    // }
+
     @Bean
-    public Map<String, Class<? extends Robot>> robotTypes() {
+    public Map<String, Class<? extends Robot>> robotTypes(ListableBeanFactory beanFactory) {
         Map<String, Class<? extends Robot>> robotTypes = new HashMap<>();
-        robotTypes.put("HAL9000", Hal9000.class);
-        // robotTypes.put("TACHIKOMAS", Tachikomas.class);
-        // robotTypes.put("JOHNNY5", Johnny5.class);
-        // robotTypes.put("MASCHINENMENSCH", Maschinenmensch.class);
-        // Add more robot models here
+
+        Map<String, Robot> robotBeans = beanFactory.getBeansOfType(Robot.class);
+        for (Map.Entry<String, Robot> entry : robotBeans.entrySet()) {
+            String modelName = entry.getKey().toUpperCase();
+            Class<? extends Robot> modelClass = entry.getValue().getClass();
+            robotTypes.put(modelName, modelClass);
+        }
+
         return robotTypes;
     }
 
