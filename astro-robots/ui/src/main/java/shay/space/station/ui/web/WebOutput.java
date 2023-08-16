e37655b1693e721fbb2ddca1a39c4b1f538eec95
“ShayM” <shay1mualem@gmail.com>
2023-08-16 17:22:12 +0300

    not need this old file

---
+++ astro-robots/ui/src/main/java/shay/space/station/ui/web/WebOutput.java
@@ -0,0 +1,18 @@
+// package shay.space.station.ui.web;
+//
+// import org.springframework.stereotype.Component;
+// import shay.space.station.ui.infra.Output;
+//
+// @Component
+// public class WebOutput implements Output {
+//     private String message;
+//
+//     @Override
+//     public void print(final String message) {
+//         this.message = message;
+//     }
+//
+//     public String getMessage() {
+//         return message;
+//     }
+// }
